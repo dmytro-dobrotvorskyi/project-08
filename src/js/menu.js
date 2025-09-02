9b4e12a4d0d83c4b8d41d3631cecc9394fbce30a
Viktor <143907417+Viktor-Yashchuk@users.noreply.github.com>
2025-09-02 21:16:45 +0200

    fix-mob-js

--- src/js/menu.js
+++ src/js/menu.js
@@ -1,18 +1,16 @@
 (() => {
-    const refs = {
-      // Додати атрибут data-modal-open на кнопку відкриття
-      openModalBtn: document.querySelector("[data-mobile-open]"),
-      // Додати атрибут data-modal-close на кнопку закриття
-      closeModalBtn: document.querySelector("[data-mobile-close]"),
-      // Додати атрибут data-modal на бекдроп модалки
-      modal: document.querySelector("[data-mobile]"),
-    };
-  
-    refs.openModalBtn.addEventListener("click", toggleModal);
-    refs.closeModalBtn.addEventListener("click", toggleModal);
-  
-    function toggleModal() {
-      // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
-      refs.modal.classList.toggle("is-open");
-    }
-  })();
+  const refs = {
+    openModalBtn: document.querySelector('[data-mobile-open]'),
+    closeModalBtn: document.querySelector('[data-mobile-close]'),
+    modal: document.querySelector('[data-mobile]'),
+    link: document.querySelectorAll('.mobile-menu-nav-link'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.link.forEach(link => link.addEventListener('click', toggleModal));
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-open');
+  }
+})();
